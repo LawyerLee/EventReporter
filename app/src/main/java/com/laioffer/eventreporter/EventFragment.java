96d78d292dc1e50aba9ab9c38dbba506f7527621
LawyerLee <lilinfengsyl@gmail.com>
2018-01-30 16:14:58 -0500

    Initial commit

---
+++ app/src/main/java/com/laioffer/eventreporter/EventFragment.java
@@ -0,0 +1,84 @@
+package com.laioffer.eventreporter;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EventFragment extends Fragment {
+    OnItemSelectListener mCallback;
+    ListView mListView;
+    private ImageView mImageViewAdd;
+
+    public EventFragment() {
+        // Required empty public constructor
+    }
+
+    // Container Activity must implement this interface
+    public interface OnItemSelectListener {
+        public void onItemSelected(int position);
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mCallback = (OnItemSelectListener) context;
+        } catch (ClassCastException e) {
+            //do something
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_events, container, false);
+        mImageViewAdd = (ImageView) view.findViewById(R.id.img_event_add);
+
+        mImageViewAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent eventReportIntent = new Intent(getActivity(), EventReportActivity.class);
+                startActivity(eventReportIntent);
+            }
+        });
+
+        return view;
+    }
+
+    public void onItemSelected(int position){
+        for (int i = 0; i < mListView.getChildCount(); i++){
+            if (position == i) {
+                mListView.getChildAt(i).setBackgroundColor(Color.BLUE);
+            } else {
+                mListView.getChildAt(i).setBackgroundColor(Color.parseColor("#FAFAFA"));
+            }
+        }
+    }
+
+    private String[] getEventNames() {
+        String[] names = {
+                "Event1", "Event2", "Event3",
+                "Event4", "Event5", "Event6",
+                "Event7", "Event8", "Event9",
+                "Event10", "Event11", "Event12"};
+        return names;
+    }
+
+}
